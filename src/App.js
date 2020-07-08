5528e11022f6c080732dd1a656634132d92603d4
Taro Moberly <ctmoberly@gmail.com>
2020-07-08 22:57:00 +0900

    added simple gsap animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import InputBox from './components/InputBox';
 import TodoItem from './components/TodoItem';
+import { TweenMax, Power3 } from 'gsap';
 
 function App() {
   const [items, setItems] = useState([]);
+
+  const inputItem = useRef();
+
+  useEffect(() => {
+    TweenMax.to(inputItem.current, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
+  });
 
   function addNewItem(item) {
     setItems((prevState) => {
@@ -28,7 +39,7 @@ function App() {
 
   return (
     <div className="container">
-      <div className="wrapper">
+      <div ref={inputItem} className="wrapper">
         <InputBox onAdd={addNewItem} />
         <div className="todoList">
           {items.map((item, index) => (
