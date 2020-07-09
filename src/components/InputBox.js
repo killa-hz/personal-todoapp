7716d616b7485e7dd9f057a2bb0bade859a91f0b
Taro Moberly <ctmoberly@gmail.com>
2020-07-09 22:34:00 +0900

    more gsap animation

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { TweenMax, Power3 } from 'gsap';
 
 const InputBox = (props) => {
   const [input, setInput] = useState('');
+  const formRef = useRef();
+  const buttonRef = useRef();
 
   function handleInput(e) {
     setInput(e.target.value);
@@ -13,17 +16,42 @@ const InputBox = (props) => {
     setInput('');
   }
 
+  function handleFocus() {
+    TweenMax.to(formRef.current, 0.2, {
+      css: { fontSize: '1.6rem' },
+      ease: Power3.easeOut,
+    });
+    TweenMax.to(buttonRef.current, 0.2, {
+      css: { fontSize: '1.6rem' },
+      ease: Power3.easeOut,
+    });
+  }
+
+  function handleFocusOut() {
+    TweenMax.to(formRef.current, 0.2, {
+      css: { fontSize: '1.4rem' },
+      ease: Power3.easeOut,
+    });
+    TweenMax.to(buttonRef.current, 0.2, {
+      css: { fontSize: '1.4rem' },
+      ease: Power3.easeOut,
+    });
+  }
+
   return (
     <div className="inputBox">
       <form>
         <input
+          ref={formRef}
           className="todoInput"
           type="text"
           placeholder="What do you need to do?"
           onInput={handleInput}
           value={input}
+          onFocus={handleFocus}
+          onBlur={handleFocusOut}
         />
-        <button className="todoButton" onClick={handleClick}>
+        <button ref={buttonRef} className="todoButton" onClick={handleClick}>
           Get it!
         </button>
       </form>
